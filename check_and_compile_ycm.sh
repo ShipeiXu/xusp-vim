ecb5f69588571d3fed2a50c18c2e0c7a29ac207b
许式佩 <shipei.xu@sinobbd.com>
2018-05-07 10:47:41 +0800

    添加自动更新vim及依赖，自动安装ycm的脚本

---
+++ check_and_compile_ycm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: check_and_compile_ycm.sh
+#
+#         USAGE: ./check_and_compile_ycm.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: ShipeiXu (xusp), xushipei2009@163.com
+#  ORGANIZATION: vpn
+#       CREATED: 2018/05/07 10 ±42∑÷11√Î
+#      REVISION:  ---
+#===============================================================================
+
+
---
+++ check_and_install_vim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: check_and_install_vim.sh
+#
+#         USAGE: ./check_and_install_vim.sh
+#
+#   DESCRIPTION: check the linux or mac env that the vim is sutiable for vim_script
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: ShipeiXu (xusp), xushipei2009@163.com
+#  ORGANIZATION: vpn
+#       CREATED: 2018/05/07 10 ±39∑÷16√Î
+#      REVISION:  ---
+#===============================================================================
+
+
