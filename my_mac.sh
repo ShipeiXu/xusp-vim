8e794899ffd1402d6e5ea21df8795b8c233651bb
xusp <xushipei2009@163.com>
2018-11-18 15:48:11 +0800

    update

--- my_mac.sh
+++ my_mac.sh
@@ -19,13 +19,50 @@
 
 set -o nounset                                  # Treat unset variables as an error
 
-#1. install brew
+function install() 
+{
+    echo install 
+    #1. install iterms  manual
 
-#2. install iterms
+    #2. install brew
+    cd
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#3. insall my vim(and plugins)
-./check_and_install_vim.sh
-./check_and_compile_ycm.sh
+    # install oh_my_zsh
+    cd 
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#4. set my color
+    #install shadowsocks 
+
+    #install proxiifier
+
+    # install python3 (mac is python2)
+
+    #3. insall my vim(and plugins)
+    ./check_and_install_vim.sh
+    ./check_and_compile_ycm.sh
+
+
+    #4. set my color
+    # 下载字体并安装
+    cd ~/work
+    git clone https://github.com/powerline/fonts.git
+    cd fonts
+    ./install.sh
+    cd
+    # 设置solarize主题
+    # 设置om my zsh的主题 agnoster
+    # 设置ls颜色 到 ~/.zshrc
+    echo 'export CLICOLOR=1' >> ~/.zshrc 
+    echo 'export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD' >> ~/.zshrc
+}
+
+function uninstall()
+{
+    echo uninstall 
+}
+
+
+uninstall 
+
 
